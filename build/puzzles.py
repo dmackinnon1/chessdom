5259267d49a2af69599afff8ec00466880feab98
dmackinnon1 <dmackinnon1@gmail.com>
2018-03-24 14:00:58 -0400

    moved puzzle elements, added new puzzles

--- build/puzzles.py
+++ build/puzzles.py
@@ -65,26 +65,60 @@ def puzzle(name, size, cover, unguard, pieces):
  
 # 3. defining specific puzzles
 puzzles = []
-puzzles.append(puzzle("5 queens on 5x5", 5, "true", "true", [piece("queen",5)]))
-puzzles.append(puzzle("5 rooks on 5x5", 5, "true", "true", [piece("rook",5)]))
+#queens domination
+puzzles.append(puzzle("2 queens on 4x4",4, "true", "false", [piece("queen",2)]))
+puzzles.append(puzzle("3 queens on 5x5", 5, "true", "false", [piece("queen",3)]))
+puzzles.append(puzzle("3 queens on 6x6", 6, "true", "false", [piece("queen",3)]))
 puzzles.append(puzzle("4 queens on 5x5", 5, "true", "true", [piece("queen",4)]))
+puzzles.append(puzzle("4 queens on 7x7", 7, "true", "true", [piece("queen",4)]))
 puzzles.append(puzzle("5 queens on 8x8", 8, "true", "true", [piece("queen",5)]))
+
+#queens independence
+puzzles.append(puzzle("4 queens on 4x4", 4, "true", "true", [piece("queen",4)]))
+puzzles.append(puzzle("5 queens on 5x5", 5, "true", "true", [piece("queen",5)]))
 puzzles.append(puzzle("6 queens on 6x6", 6, "true", "true", [piece("queen",6)]))
-puzzles.append(puzzle("5 knights on 5x5", 5, "true", "true", [piece("knight",5)]))
+puzzles.append(puzzle("7 queens on 7x7", 7, "true", "true", [piece("queen",7)]))
+puzzles.append(puzzle("8 queens on 8x8", 8, "true", "true", [piece("queen",8)]))
+
+#knight domination
+puzzles.append(puzzle("4 knights on 4x4", 4, "true", "true", [piece("knight",4)]))
+puzzles.append(puzzle("8 knights on 6x6", 4, "true", "true", [piece("knight",8)]))
+puzzles.append(puzzle("10 knights on 7x7", 7, "true", "false", [piece("knight",10)]))
 puzzles.append(puzzle("12 knights on 8x8", 8, "true", "false", [piece("knight",12)]))
+
+#knight independence
+puzzles.append(puzzle("5 knights on 3x3", 3, "true", "true", [piece("knight",5)]))
+puzzles.append(puzzle("8 knights on 4x4", 4, "true", "true", [piece("knight",8)]))
+puzzles.append(puzzle("5 knights on 5x5", 5, "true", "true", [piece("knight",5)]))
 puzzles.append(puzzle("12 knights on 5x5", 5, "true", "true", [piece("knight",12)]))
+puzzles.append(puzzle("13 knights on 5x5", 5, "true", "true", [piece("knight",13)]))
+puzzles.append(puzzle("18 knights on 6x6", 6, "true", "true", [piece("knight",18)]))
+puzzles.append(puzzle("25 knights on 7x7", 7, "true", "true", [piece("knight",25)]))
+
+#rook domination
+puzzles.append(puzzle("4 rooks on 4x4", 4, "true", "true", [piece("rook",4)]))
+puzzles.append(puzzle("5 rooks on 5x5", 5, "true", "true", [piece("rook",5)]))
+puzzles.append(puzzle("6 rooks on 6x6", 6, "true", "true", [piece("rook",6)]))
+
+#bishop independence
 puzzles.append(puzzle("14 bishops on 8x8", 8, "true", "true", [piece("bishop",14)]))
 puzzles.append(puzzle("12 bishops on 7x7", 7, "true", "true", [piece("bishop",12)]))
 puzzles.append(puzzle("8 bishops on 5x5", 5, "true", "true", [piece("bishop",8)]))
 puzzles.append(puzzle("6 bishops on 4x4", 4, "true", "true", [piece("bishop",6)]))
-puzzles.append(puzzle("9 kings on 5x5", 5, "true", "true", [piece("king",9)]))
-puzzles.append(puzzle("9 kings on 8x8", 8, "true", "true", [piece("king",9)]))
 puzzles.append(puzzle("6 bishops on 5x5", 5, "true", "true", [piece("bishop",6)]))
 puzzles.append(puzzle("10 bishops on 7x7", 7, "true", "true", [piece("bishop",10)]))
 puzzles.append(puzzle("10 bishops on 8x8", 8, "true", "true", [piece("bishop",10)]))
-puzzles.append(puzzle("3 queens on 5x5", 5, "true", "false", [piece("queen",3)]))
-puzzles.append(puzzle("3 queens on 6x6", 6, "true", "false", [piece("queen",3)]))
-puzzles.append(puzzle("2 queens on 4x4",4, "true", "false", [piece("queen",2)]))
+
+#king domination
+puzzles.append(puzzle("4 kings on 5x5", 5, "true", "true", [piece("king",4)]))
+puzzles.append(puzzle("4 kings on 6x6", 6, "true", "true", [piece("king",4)]))
+
+#king independence
+puzzles.append(puzzle("9 kings on 5x5", 5, "true", "true", [piece("king",9)]))
+puzzles.append(puzzle("9 kings on 6x6", 6, "true", "true", [piece("king",9)]))
+puzzles.append(puzzle("9 kings on 7x7", 7, "true", "true", [piece("king",9)]))
+puzzles.append(puzzle("9 kings on 8x8", 8, "true", "true", [piece("king",9)]))
+
 
 # 4. printing puzzle file
 print('-------------------------------------------')
